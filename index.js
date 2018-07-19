f25caaf0ea9dbc4c8cce4fe5a4946f802492960e
Mohammed Ameen <jegroamin@gmail.com>
2018-07-19 06:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ function calculatesFarePrice(start, destination){
   else if(start <= 34 && destination >= 32){
     return 2.56
   }
+  else if(start <= 50 && destination >= 58){
+    return 25
+  }
 }
 
 
